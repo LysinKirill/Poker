547b260a031f69b69d67df623af15d83fc8dae4c
Kirill Lysin <kslysin_1@edu.hse.ru>
2024-01-12 00:39:25 +0300

    Add Table.cs

--- Poker/Table.cs
+++ Poker/Table.cs
@@ -4,6 +4,20 @@ public class Table
 {
     public List<Card> DealtCards { get; } = new List<Card>(5);
     public int NumberOfCards { get; private set; } = 0;
-    
-    public static int CompareHands(Hand)
+
+    public int CompareHands(Hand firstHand, Hand secondHand)
+    {
+        return 0;
+    }
+
+    public int CheckStraightFlush(List<Card> cards)
+    {
+        if (cards.Count < 5)
+            return 0;
+        cards = cards.OrderByDescending(card => card.CardValue).ToList();
+
+        int currConsecutive = 0;
+        //for(int i = 0; i < cards.)
+        return 1;
+    }
 }
